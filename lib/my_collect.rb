b9e52ce00d55793c48f6dc556aa196124ed2b654
Samidh Patel <spatel13456@gmail.com>
2019-01-12 01:30:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   name_collect = []
   i = 0;
   while i < array.length
-    name_collection << yield (array[i])
+    name_collection << yield(array[i])
     i += 1
   end 
   array
